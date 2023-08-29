4f8a31914108ff9b84234c018f41277eded909be
amir-kedis <amir.kedis@gmail.com>
2023-08-29 13:52:21 +0300

    🦴 Create main structure

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _printf - printf clone will behave exactly as the original
+ * @format: list of argument types passed to the function
+ * Return: number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	(void)format;
+	return (0);
+}
